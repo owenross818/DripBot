57836fb1683fb065287a365011e7ea614ac9b365
owenross818 <owen.gamer617@gmail.com>
2025-06-30 20:32:30 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,333 @@
+  const items = [
+    {
+      type: "shoes",
+        name: "Asics Gel-Kayano",
+        genre: "modern",
+        img: "https://i.imgur.com/0uPp18z.png",
+        url: "https://www.urbanoutfitters.com/shop/asics-gel-kayano-14-sneaker3?category=mens-clothing&color=034&type=REGULAR&quantity=1",
+        color: ["white","green"],
+    },
+    
+    {  
+      type: "top",
+      name: "Florida Sweatshirt",
+      genre: "modern",
+      img: "https://i.imgur.com/VZlZIJa.png",
+      url: "https://www.hollisterco.com/shop/us/p/boxy-florida-gators-national-championship-graphic-sweatshirt-59640390?categoryId=614330499&faceout=prod&seq=01",
+      color: ["black"],
+      },
+    {  
+      type: "top",
+      name: "Redsox Jersey",
+      genre: "sports",
+      img: "https://static.nike.com/a/images/t_default/57842c4b-2813-4416-ba6c-c1cd4f172c52/M+Nk+1.0+Replica+Player+Jersey.png",
+      url: "https://www.mlbshop.com/boston-red-sox/jerseys/t-14998553+d-9038770147+z-95-320058141",
+      color: ["white", "red"],
+      },
+    {  
+      type: "hat",
+      name: "Diamondbacks Hat - Pink",
+      genre: "sports",
+      img: "https://images.stockx.com/images/New-Era-x-Hat-Club-Arizona-Diamondbacks-20th-Anniversary-Patch-D-Strawberry-Jam-59Fifty-Fitted-Hat-Pink.jpg?fit=fill&bg=FFFFFF&w=480&h=320&q=60&dpr=1&trim=color&updated_at=1666641311",
+      url: "https://stockx.com/new-era-x-hat-club-arizona-diamondbacks-20th-anniversary-patch-d-strawberry-jam-59fifty-fitted-hat-pink",
+      color: ["gray"],
+      },
+    {  
+      type: "hat",
+      name: "White Sox Hat - Gray",
+      genre: "sports",
+      img: "https://hatstore.imgix.net/196174027360_1.jpg",
+      url: "https://www.ebay.com/itm/275938098411",
+      color: ["gray"],
+      },
+  {  
+  type: "hat",
+  name: "Brown University Hat",
+  genre: "old money",
+  img: "https://www.ivysport.com/cdn/shop/files/BRN09H18.jpg?v=1750048048",
+  url: "https://www.ivysport.com/products/brown-university-spirit-baseball-hat-one-size-brown",
+  color: ["brown"],
+  },
+  {
+  type: "hat",  
+  name: "Celtics Hat",
+  genre: "sports",
+  img: "https://hatroom.eu/images/zoom/60434970-center.jpg",
+  url: "https://hatroom.eu/en/caps/caps-new-era-boston-celtics-retro-trucker-cap-green-black/",
+  color: ["green"]
+  },
+  {
+  type: "hat",
+  name: "Purple Dodgers Hat",
+  genre: "sports",
+  img: "https://i.imgur.com/WLJnml3.png",
+   url: "https://www.mlbshop.com/los-angeles-dodgers/mens-los-angeles-dodgers-new-era-purple-vice-59fifty-fitted-hat/t-36331066+p-355500449981818+z-9-3311523937",
+  color: ["purple","teal"],
+  },
+  {
+  type: "hat",
+  name: "Blue Bucket",
+  genre: "surfer",
+  img: "https://images.squarespace-cdn.com/content/v1/5448dbc6e4b09edc336ae843/1661207412597-WZGX7AUW5BDVA9LK6OM2/IMG_8532.PNG?format=1000w",
+  url: "https://northshoresurfshop.com/accessories/nsss-knit-beanie-eblyl",
+  color: ["blue"],
+  },
+  {
+  type: "hat",
+  name: "Golf Hat",
+  genre: "country",
+  img: "https://m.media-amazon.com/images/I/41BohqYMx+L._AC_SL1000_.jpg",
+  url: "https://www.amazon.com/SHANKITGOLF-Tiger-Golf-Adjustable-Funny/dp/B0BMS68GDC",
+  color: ["white"],
+  },
+  {
+  type: "hat",
+  name: "Fedora",
+  genre: "old money",
+  url: "https://heromoosehats.com/products/mens-crushable-black-felt-fedora",
+  img: "https://i.imgur.com/BGA9MJH.png",
+  color: ["black"],
+  },
+  {  
+  type: "hat", 
+  name: "Padres City Connect Hat",
+  genre: "sports",
+  url: "https://www.neweracap.com/products/san-diego-padres-city-connect-59fifty-fitted",
+  img: "https://www.neweracap.com/cdn/shop/products/60231697_59FIFTY_MLB21CITYCNCTOFF_SADPAD_BLUE_F.png?v=1655929822&width=1946",  
+  color: ["teal"],
+  },
+  {
+  type: "hat",
+  name: "Redsox Hat",
+  genre: "sports",
+  url: "https://sneakerpolitics.com/products/new-era-x-politics-boston-red-sox-59fifty-fitted-hat-navy",
+  img: "https://sneakerpolitics.com/cdn/shop/files/Sneaker-Politics-Politics-NewEra-RedSox-WB-1.jpg?v=1720625977&width=480",
+  color: ["red"],
+  },
+  {
+  type: "top",
+  name: "Mpabbe Jersey",
+  genre: "sports",
+  url: "https://soccerzoneusa.com/products/kids-replica-nike-mbappe-france-home-jersey-2022", 
+  img: "https://soccerzoneusa.com/cdn/shop/products/preview-98_15c56d65-0f87-4d58-ac8d-944eb77cbb3f_2000x.jpg?v=1668970448",
+  color: ["blue"],
+  },
+  {
+  type: "top",
+  name: "Hawaiian Shirt",
+  genre: "tropical",
+  url: "https://www.amazon.com/SEAOYOZO-Hawaiian-Shirts-Holiday-Flamingo/dp/B0D2HT2KWP?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&smid=A3DBIMPJI82T8W&gQT=1&th=1&psc=1",
+  img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRcH6DexJktQVY7gEHOAJPPEMnYuk_iDxT6OdPd0TEpSi8nu6VKk0ypbbcQ94-lrRKMGdlhkJ_bxlEXdsonjwGT9wXph8DxWglPqp4DH_mQu6QZ7GLYilzl",
+  color: ["teal"],
+  },
+  {
+  type: "top", 
+  name: "Flannel Shirt",
+  genre: "country",
+  url: "https://www.llbean.com/llb/shop/42272?page=scotch-plaid-flannel-shirt-traditional-fit",
+  img: "https://cdni.llbean.net/is/image/wim/228061_54844_44?hei=1095&wid=950&resMode=sharp2&defaultImage=llbprod/228061_54844_41",
+  color: ["brown"],
+  },
+  {
+  type: "top",
+  name: "Polo Shirt",
+  genre: "old money",
+  url: "https://www.magnattos.com/products/mens-shirt-lapel-summer-short-sleeved-new-style-for-work-daily-casual-breathable-comfortable-simple-and-fashionable?variant=48803040330034",
+  img: "https://www.magnattos.com/cdn/shop/files/seibel_28.png?v=1717198688",
+  color: ["green"],
+  },
+  {
+  type: "top",
+  name: "Vintage Tee",
+  genre: "skater",
+  img: "https://shop.bobmarley.com/cdn/shop/products/MARLEY_SKATE_IRON-LION-ZION-TEE-1_91f6e02a-496d-43b7-a52f-b6ab3bb2df96.png?v=1585837473",
+  url: "https://shop.bobmarley.com/products/vintage-iron-lion-zion-t-shirt",
+  color: ["gray"],
+  },
+  {
+  type: "bottom",
+  name: "Khaki Shorts",
+  genre: "old money",
+  url: "https://kahala.com/products/kealoha-short?variant=41135575269433",
+  img: "https://kahala.com/cdn/shop/files/3634-KS38-251_1800x1800.png?v=1691457602",
+  color: ["brown"],
+  },
+  {
+  type: "bottom",
+  name: "Baggy Jeans",
+  genre: "Y2K",
+  url: "https://www.hollisterco.com/shop/us/p/medium-wash-super-baggy-jeans-58661022?seq=02&utm_source=googleshopping",
+  img: "https://www.shutterstock.com/image-photo/fashion-trendy-womens-jeans-isolated-600nw-2466665519.jpg",
+  color: ["teal"],
+  },
+  {
+  type: "bottom",
+  name: "Cargo Pants",
+  genre: "modern",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTFO2Q39KwLFpjCKtqUEgEMUjfZ8XD2OkO6Q&s",
+  url: "https://www.amazon.com/Outdoor-Straight-Waisted-Military-Pockets/dp/B0BZMJXMYK",
+  color: ["green"],
+  },
+  {
+  type: "bottom",
+  name: "Jorts",
+  genre: "skater",
+  img: "https://cdn11.bigcommerce.com/s-brinfq1ux4/images/stencil/1280x1280/products/2484748/335046/4salijorts03dbb__38476.1698408944.png?c=2",
+  url: "https://www.boardparadise.com/alien-workshop-jorts-shorts-l-denim-blue/",
+  color: ["blue"],
+  },
+  {
+  type: "bottom",
+  name: "Sweatpants",
+  genre: "lounge",
+  img: "https://scene7.zumiez.com/is/image/zumiez/product_main_medium/Nike-Sportswear-Kids-Club-Black-Jogger-Sweatpants-_379509.jpg",
+  url: "https://www.wegotsoccer.com/item/nike-team-club-pant/w24631/bkw",
+  color: ["black"],
+  },
+  {
+  type: "shoes",
+  name: "Loafers",
+  genre: "old money",
+  url: "https://doublemonk.com/products/boston-burgundy-cavalry-calf",
+  img: "https://doublemonk.com/cdn/shop/products/CJ-Boston-Burgundy-Cavalry-Calf-Front_1200x900.jpg?v=1628138897",
+  color: ["brown"],
+  },
+  {
+  type: "shoes",
+  name: "Vans",
+  genre: "skater",
+  url: "https://newstaractive.com/collections/mens-shoes/products/vn-0d3hy28",
+  img: "https://newstaractive.com/cdn/shop/products/vans_oldskool-blkwhtshop3_2da57fbe-078c-4bf2-a95d-763bdf074974.jpg?v=1490387878",
+  color: ["white", "black"],
+  },
+  {
+  type: "shoes",
+  name: "NewBalance 480s - Navy/White",
+  genre: "modern",
+  url: "https://www.flightclub.com/numeric-480-navy-white-nm480dnv",
+  img: "https://cdn.flightclub.com/TEMPLATE/385203/1.jpg",
+  color: ["gray"],
+  },
+  {
+  type: "shoes",
+  name: "Birkenstocks",
+  genre: "lounge",
+  url: "https://www.birkenstock.com/us/boston-soft-footbed-suede-leather/boston-suede-suedeleather-softfootbed-eva-u_90.html",
+  img: "https://www.birkenstock.com/dw/image/v2/BDXC_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4c303a0a/660461/660461.jpg?sw=848&sh=848&strip=false",
+  color: ["brown"],
+  },
+  { 
+  type: "shoes",
+  name: "NewBalance 2002s - Black",
+  genre: "modern",
+  url: "https://www.ebay.com/b/New-Balance-2002R-JD-Exclusive-Black/15709/bn_7121984185",
+  img: "https://i.ebayimg.com/images/g/nIgAAOSwea5nuo60/s-l1600.jpg",
+  color: ["black","gray"],
+  },
+  {
+  type: "shoes",
+  name: "Sambas",
+  genre: "modern",
+  url: "https://www.adidas.com/us/handball-spezial-shoes/BD7633.html",
+  img: "https://d2bzx2vuetkzse.cloudfront.net/fit-in/0x450/unshoppable_producs/39742162-1bc9-4f59-ad32-33a762df6dd1.png", 
+  color: ["purple"],
+  },
+  ];
+  const compatibleColors = {
+    white: ["white", "gray","orange","red","yellow","green","blue","purple","black","brown","teal","pink"],
+    black: ["white", "gray","yellow","teal","pink","orange","green","red","black"],
+    gray: ["white", "black","blue","teal","green","red","yellow","orange","purple","blue","pink","gray","brown"],
+    blue: ["white", "gray", "pink", "red", "yellow", "orange","blue"],
+    purple: ["white","gray","yellow","purple"],
+    green: ["black","brown","white","gray","green"],
+    yellow: ["white","gray","black","blue","purple","pink","yellow"],
+    orange: ["white", "gray", "black", "blue"],
+    red: ["white", "gray", "black", "blue","orange"],
+    teal: ["white", "gray", "black", "pink","teal"],
+    pink: ["white", "gray", "black", "blue","yellow","teal","pink"],
+    brown: ["white", "gray", "green","brown","orange"],
+  }
+function getCompatibleItem(type, matchColors) {
+const candidates = items.filter(item => item.type === type);
+
+if (!matchColors || matchColors.length === 0) {
+// If it's the first item, just return any item of that type
+return candidates[Math.floor(Math.random() * candidates.length)];
+}
+
+// Combine all compatible colors from all matched colors
+const allowedColors = new Set();
+matchColors.forEach(color => {
+const matches = compatibleColors[color] || [];
+matches.forEach(c => allowedColors.add(c));
+});
+
+const compatible = candidates.filter(item =>
+item.color.some(c => allowedColors.has(c))
+);
+
+// Fallback if no compatible items found
+if (compatible.length === 0) return candidates[Math.floor(Math.random() * candidates.length)];
+
+return compatible[Math.floor(Math.random() * compatible.length)];
+}
+  function generateOutfit() {
+  const hat = getCompatibleItem("hat",);
+  const top = getCompatibleItem("top", hat.color);
+  const bottom = getCompatibleItem("bottom", top.color);
+  const shoes = getCompatibleItem("shoes", bottom.color);
+
+  const genres = [hat.genre, top.genre, bottom.genre, shoes.genre];
+  const vibe = mostCommonGenre(genres);
+
+  const stackedImages = `
+  <div class="outfit-vibe">vibe: ${vibe}</div>
+  <div class="outfit-stack">
+  ${renderImage(hat)}
+  ${renderImage(top)}
+  ${renderImage(bottom)}
+  ${renderImage(shoes)}
+  </div>
+  <div class="buy-links">
+  ${renderLink(hat)}
+  ${renderLink(top)}
+  ${renderLink(bottom)}
+  ${renderLink(shoes)}
+  </div>
+  `;
+
+  document.getElementById("outfit").innerHTML = stackedImages;
+  }
+
+  function renderImage(item) {
+  return `
+  <div class="outfit-piece">
+  <img src="${item.img}" alt="${item.name}">
+  </div>
+  `;
+  }
+
+  function renderLink(item) {
+  return `<a href="${item.url}" target="_blank">Buy ${item.name}</a>`;
+  }
+
+  function getRandom(type) {
+  const filtered = items.filter(item => item.type === type);
+  return filtered[Math.floor(Math.random() * filtered.length)];
+  }
+
+  function mostCommonGenre(genres) {
+  return genres.sort((a, b) =>
+  genres.filter(g => g === a).length - genres.filter(g => g === b).length
+  ).pop();
+  }
+
+  function renderItem(item) {
+  return `
+  <div class="outfit-item">
+  <img src="${item.img}" alt="${item.name}">
+  <p><strong>${item.name}</strong></p>
+  <a href="${item.url}" target="_blank">Buy Now</a>
+  </div>
+  `;
+  }
